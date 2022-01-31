07a6be4ffb3b8148b3e67536d2b66e7a88cceeb5
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2022-01-31 17:31:31 +0100

    Creata la base per la carta

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,21 +2,32 @@
     <div class="content">
         <Job :lingua="lingua"/>
         <Youtube :lingua="lingua"/>
+        <Preset 
+        :lingua="lingua"
+
+        />
     </div>
 </template>
 
 <script>
 import Job from './Job.vue'
 import Youtube from './Youtube.vue'
+import Preset from './Preset.vue'
 
 export default {
     name: 'Content',
     components: {
         Job,
-        Youtube
+        Youtube,
+        Preset
     },
     props: {
         lingua: String
+    },
+    data() {
+        return {
+            
+        }
     }
 }
 </script>
---
+++ src/components/Preset.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="preSetContainer">
+      <div class="card">
+          <h2 class="title">PreSet name</h2>
+          <img src="" alt="PreSet screen">
+          <p>Pre set short description of about 50 words</p>
+          <p>Preset popularity from 1 to 5 stars</p>
+      </div>
+      <div class="card">
+          <h2 class="title">PreSet name</h2>
+          <img src="" alt="PreSet screen">
+          <p>Pre set short description of about 50 words</p>
+          <p>Preset popularity from 1 to 5 stars</p>
+      </div>
+      <div class="card">
+          <h2 class="title">PreSet name</h2>
+          <img src="" alt="PreSet screen">
+          <p>Pre set short description of about 50 words</p>
+          <p>Preset popularity from 1 to 5 stars</p>
+      </div>
+      <div class="card">
+          <h2 class="title">PreSet name</h2>
+          <img src="" alt="PreSet screen">
+          <p>Pre set short description of about 50 words</p>
+          <p>Preset popularity from 1 to 5 stars</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Preset',
+    props: {
+        lingua: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/common.scss';
+.preSetContainer {
+    display: flex;
+    justify-content: space-around;
+    padding: 30px;
+    background-image: linear-gradient($opposite_color, black);
+}
+.card  {
+    display: inline-block;
+    width: 200px;
+    height: 300px;
+    padding: 5px;
+    border: 2px solid black;
+    background-image: linear-gradient(black, $opposite_color);
+    color: white;
+}
+</style>

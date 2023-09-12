be8b534b3565582c267e842bf5d8dfdf6cf8457d
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2023-09-12 21:30:53 +0200

    Added the renata's website

--- src/components/Preset.vue
+++ src/components/Preset.vue
@@ -1,30 +1,34 @@
 <template>
-  <div class="preSetContainer">
-      <div class="card">
-          <h2 class="title">PreSet name</h2>
-          <img src="" alt="PreSet screen">
-          <p>Pre set short description of about 50 words</p>
-          <p>Preset popularity from 1 to 5 stars</p>
-      </div>
-      <div class="card">
-          <h2 class="title">PreSet name</h2>
-          <img src="" alt="PreSet screen">
-          <p>Pre set short description of about 50 words</p>
-          <p>Preset popularity from 1 to 5 stars</p>
-      </div>
-      <div class="card">
-          <h2 class="title">PreSet name</h2>
-          <img src="" alt="PreSet screen">
-          <p>Pre set short description of about 50 words</p>
-          <p>Preset popularity from 1 to 5 stars</p>
-      </div>
-      <div class="card">
-          <h2 class="title">PreSet name</h2>
-          <img src="" alt="PreSet screen">
-          <p>Pre set short description of about 50 words</p>
-          <p>Preset popularity from 1 to 5 stars</p>
-      </div>
-  </div>
+    <div class="preSetContainer">
+        <div class="card">
+            <h2 class="title">Renata's Website</h2>
+            <div>
+                <a href="https://flamboyant-spence-68b208.netlify.app/">
+                    <img src="../assets/Screenshot 2023-09-12 210128.png" alt="Renata screenshot">
+                </a>
+            </div>
+            <p>Pre set short description of about 50 words</p>
+            <p>Preset popularity from 1 to 5 stars</p>
+        </div>
+        <!-- <div class="card">
+            <h2 class="title">PreSet name</h2>
+            <img src="" alt="PreSet screen">
+            <p>Pre set short description of about 50 words</p>
+            <p>Preset popularity from 1 to 5 stars</p>
+        </div>
+        <div class="card">
+            <h2 class="title">PreSet name</h2>
+            <img src="" alt="PreSet screen">
+            <p>Pre set short description of about 50 words</p>
+            <p>Preset popularity from 1 to 5 stars</p>
+        </div>
+        <div class="card">
+            <h2 class="title">PreSet name</h2>
+            <img src="" alt="PreSet screen">
+            <p>Pre set short description of about 50 words</p>
+            <p>Preset popularity from 1 to 5 stars</p>
+        </div> -->
+    </div>
 </template>
 
 <script>
@@ -38,13 +42,15 @@ export default {
 
 <style lang="scss" scoped>
 @import '../style/common.scss';
+
 .preSetContainer {
     display: flex;
     justify-content: space-around;
     padding: 30px;
     background-image: linear-gradient($opposite_color, black);
 }
-.card  {
+
+.card {
     display: inline-block;
     width: 200px;
     height: 300px;
@@ -52,5 +58,12 @@ export default {
     border: 2px solid black;
     background-image: linear-gradient(black, $opposite_color);
     color: white;
+
+    a {
+        img {
+            width: 100%;
+        }
+    }
+
 }
 </style>
